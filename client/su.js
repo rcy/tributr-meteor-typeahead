92a085ac9d39e822934a9f682cd4ac93ba4cd403
Ryan Yeske <rcyeske@gmail.com>
2013-09-12 22:13:13 -0700

    initial import

---
+++ client/su.js
@@ -0,0 +1,22 @@
+Users = new Meteor.Collection(null);
+
+Meteor.startup(function() {
+  Users.insert({ name: 'bob' });
+  Users.insert({ name: 'mary' });
+  Users.insert({ name: 'sam' });
+});
+
+Session.set('select.user.focus', false);
+
+Template.selectUser.events({
+  'keyup input': function(e, t) {
+    Session.set('select.user.input', $(e.target).val());
+  }
+});
+
+Template.selectUser.helpers({
+  options: function() {
+    var query = Session.get('select.user.input');
+    return Users.find({name: query});
+  }
+});
