230ce1a5943e37d94da58855c0fb25be813eeb65
Ryan Yeske <rcyeske@gmail.com>
2013-09-12 22:26:38 -0700

    fix query bug, and add names

--- client/su.js
+++ client/su.js
@@ -2,9 +2,12 @@ Users = new Meteor.Collection(null);
 
 Meteor.startup(function() {
   Users.insert({ name: 'bob' });
+  Users.insert({ name: 'beatrice' });
   Users.insert({ name: 'mary' });
+  Users.insert({ name: 'mike' });
   Users.insert({ name: 'sam' });
   Users.insert({ name: 'ephraim' });
+  Users.insert({ name: 'eugene' });
   Users.insert({ name: 'ryan' });
   Users.insert({ name: 'geoff' });
 });
@@ -20,7 +23,7 @@ Template.selectUser.events({
 Template.selectUser.helpers({
   options: function() {
     var query = Session.get('select.user.input');
-    if (query.length) {
+    if (query && query.length) {
       var re = '^' + query;
       return Users.find({name: {$regex: re}});
     }
